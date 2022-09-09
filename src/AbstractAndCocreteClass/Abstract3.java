3f852de9e52291a7fa24c8427b71284eea9d53cb
TSuhas <suhastupake07@gmail.com>
2022-09-09 17:28:00 +0530

    tenth commit

---
+++ src/AbstractAndCocreteClass/Abstract3.java
@@ -0,0 +1,14 @@
+package AbstractAndCocreteClass;
+
+public abstract class Abstract3 {
+public abstract void  a1();
+	
+	public static void a2() {
+		System.out.println("Print a2 method ");
+	}
+	public  abstract void a3();
+	
+	public  void a4() {
+		System.out.println("Print a4 method");
+	}
+}
---
+++ src/AbstractAndCocreteClass/ConcreteClass.java
@@ -0,0 +1,20 @@
+package AbstractAndCocreteClass;
+
+public class ConcreteClass extends Abstract3 {
+
+	@Override
+	public void a1() {
+		// TODO Auto-generated method stub
+		System.out.println("Print a1 method");
+	}
+
+	@Override
+	public void a3() {
+		// TODO Auto-generated method stub
+		System.out.println("Print a3 method");
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
